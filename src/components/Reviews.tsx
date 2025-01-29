f58aac643edf7e9c1b2bb7dc2e4a2182a04b21b5
Umang Rathour <rathourumang@gmail.com>
2025-01-29 19:16:02 +0530

    ADDED review component

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,16 +1,58 @@
-function Reviews () {
-    return (
-        <div className='my-12 px-12'>
-        <div className='flex justify-between items-center'>
-          <div className='bg-green-600 rounded-full w-[20px] h-[7px]'></div>
-          <div className='border-[1px] mt-[-2px] mr-4 w-full'></div>
-          <h2 className='font-bold text-[24px]'>Reviews</h2>
-          <div className='border-[1px] mt-[-2px] ml-4 w-full'></div>
-          <div className='bg-green-600 rounded-full w-[20px] h-[7px]'></div>
-        </div>
+import { useState, useEffect } from 'react';
+import { reviews } from '../utils/reviews';
+import { motion, AnimatePresence } from 'framer-motion';
 
+const Reviews = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleNext();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === reviews.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  return (
+    <div className='my-12 p-12 h-fit'>
+      <div className='flex justify-between items-center'>
+        <div className='bg-green-600 rounded-full w-[20px] h-[7px]'></div>
+        <div className='border-[1px] mt-[-2px] mr-4 w-full'></div>
+        <h2 className='font-bold text-[24px]'>Reviews</h2>
+        <div className='border-[1px] mt-[-2px] ml-4 w-full'></div>
+        <div className='bg-green-600 rounded-full w-[20px] h-[7px]'></div>
+      </div>
+
+      <div className='relative mt-8'>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={currentIndex}
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -100 }}
+            transition={{ duration: 0.5 }}
+            className='text-center'
+          >
+            <p className='mt-4 font-bold'>{reviews[currentIndex].author}</p>
+            <p className='mt-4'>{reviews[currentIndex].text}</p>
+          </motion.div>
+        </AnimatePresence>
+        
+        <div className='right-0 bottom--8 left-0 absolute flex justify-center mt-4'>
+          {reviews.map((_, index) => (
+            <div
+              key={index}
+              className={`w-2 h-2 mx-1 rounded-full ${index === currentIndex ? 'bg-green-600' : 'bg-gray-400'}`}
+              onClick={() => setCurrentIndex(index)}
+            ></div>
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Reviews;
---
+++ src/utils/reviews.ts
@@ -0,0 +1,14 @@
+export const reviews = [
+  {
+   text: 'Wonderful Job from Umang. I am very happy with his problem solving skills and ability to come up with quick and effective solutions. He cared deeply for my project and treated it as his own.',
+    author: 'Jaden R.'
+  },
+  {
+    text: 'Umang really knows what he is doing about and is great to work with. All gives good suggestions to improve the overall project. This is my second project him. He is great.',
+    author: 'Jatinder K.'
+  },
+  {
+    text: 'Umang is a great hand. He understood the assignment properly and was always striving to do the job better.',
+    author: 'Jenny Kaur.'
+  }
+];
