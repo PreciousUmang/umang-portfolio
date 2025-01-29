709440359e7a042cda3b84a82c081e94cc286b4e
Umang Rathour <rathourumang@gmail.com>
2025-01-29 12:27:01 +0530

    Fixed vite config and added new dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
-  base: "/umang-portfolio/"
-=======
   base: '/umang-portfolio',
->>>>>>> 7e10ba07f40afc52ac1973da283a769bb433bde1
 })
