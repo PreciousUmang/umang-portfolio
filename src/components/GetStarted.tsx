d7d1f13e76d167de570c32701cb822a547499bd4
Umang Rathour <rathourumang@gmail.com>
2025-01-30 14:35:28 +0530

    Refactor GetStarted component styles and update CSS for better responsiveness

--- src/components/GetStarted.tsx
+++ src/components/GetStarted.tsx
@@ -19,7 +19,7 @@ function GetStarted () {
         <div className='mt-[-2px] ml-4 border-b-[1px] w-1/2'></div>
         <div className='bg-green-600 rounded-full w-[7px] h-[7px]'></div>
       </div>
-      <h1  className='my-8 px-16 text-center'>Ready to get started in building something exceptional?</h1>
+      <h1  className='my-5 font-semibold text-center text-wrap sm:text-[40px] md:text-[60px] lg:text-[70px]'>Ready to get started in building something exceptional?</h1>
       <MotionButton/>
     </motion.div>
   )
